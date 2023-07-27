a4b67c3c8ff924a780f52dabaf7661316d937698
Lucas Ferreiro <lferreiro.823@gmail.com>
2023-07-27 12:25:10 -0300

    Crear rutinas de 1,2 y 3 dias (solo series)

--- js/rutina.js
+++ js/rutina.js
@@ -1,5 +1,7 @@
 let rutinaForm = document.getElementById("rutina");
 let btn_crear = document.createElement("button");
+let resultado = document.querySelector("#resultado");
+let tabla_dia =[];
 let musculos = [
     {
         "nombre" : "pecho",
@@ -174,7 +176,93 @@ function crearForm() {
 btn_crear.addEventListener("click",crearRutina);
 function crearRutina(e){
     e.preventDefault();
-    musculos.forEach(element => {
-        
-    });
+    resultado.innerHTML = "";
+    tabla_dia = [];
+    let dias = document.getElementById("dias").value;
+    for(let i = 1; i <=dias; i++){
+        let table = document.createElement("table");
+        tabla_dia.push(table);
+        resultado.appendChild(table);
+        let tr  = document.createElement("tr");
+        table.appendChild(tr);
+        let th  = document.createElement("th");
+        th.innerText = "Día " + i;
+        tr.appendChild(th);
+        tr  = document.createElement("tr");
+        table.appendChild(tr);
+        th  = document.createElement("th");
+        th.innerText = "Musculo";
+        tr.appendChild(th);
+        th  = document.createElement("th");
+        th.innerText = "Series";
+        tr.appendChild(th);
+    }
+    
+    if(dias>3){
+        let piernas = false;
+        musculos.forEach(element => {
+            let valor = document.getElementById(element.nombre).value;
+            if(element.name == "cuadriceps" || element.name == "cuadriceps"
+            || element.name == "gemelos" || element.name == "soleo" || element.name == "gluteo"){
+                if(valor>0){
+                    piernas = true;
+                }
+            }
+            if(valor%2==1){
+                if(piernas){
+
+                }else{
+
+                }
+            }
+            else{
+                if(piernas){
+                    
+                }else{
+
+                }
+                for(let i = 1; i <= dias; i++){
+                    agregarSerie(element.nombre,valor/dias,i-1);
+                }
+            }
+    
+        });
+    }else{
+        musculos.forEach(element => {
+            let valor = document.getElementById(element.nombre).value;
+            res = Math.floor(valor/dias);
+            if(valor%dias==1){
+                agregarSerie(element.nombre,res,0);
+                res++;
+                for(let i = 2; i <= dias; i++){
+                    agregarSerie(element.nombre,res,i-1);
+                    res--
+                }
+            }
+            else if(valor%dias==2){
+                agregarSerie(element.nombre,res+1,0);
+                agregarSerie(element.nombre,res,1,1);
+                agregarSerie(element.nombre,res+1,2);
+            }
+            else{
+                for(let i = 1; i <= dias; i++){
+                    agregarSerie(element.nombre,valor/dias,i-1);
+                }
+            }
+        });
+    }
+    
+    
+}
+function agregarSerie(musculo, serie, dia){
+    if(serie > 0){
+        let tr  = document.createElement("tr");
+        tabla_dia[dia].appendChild(tr);
+        let td  = document.createElement("td");
+        td.innerText = musculo;
+        tr.appendChild(td);
+        td  = document.createElement("td");
+        td.innerText = serie;
+        tr.appendChild(td);
+    }
 }
